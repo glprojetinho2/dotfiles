783af69efae7e7b854607024bc9903ef33bcc349
manj <gr20052512@gmail.com>
2024-11-30 18:44:29 -0300

    adiciona autenticação do git

--- arch_based_install.sh
+++ arch_based_install.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
-# instala ferramentas do Rust
-curl --tlsv1.2 --proto "=https" https://sh.rustup.rs -Ssf | sh -s -- -y
-clear
-
 # instala o pipx
 yes y | sudo pacman -S --needed python-pip python python-pipx
 python3 -m pipx ensurepath
 clear
 
 # alguns programas essenciais
-yes y | yay -S --needed anki-bin librewolf-bin syncthing ripgrep alacritty
+yes y | yay -S --needed anki-bin librewolf-bin syncthing ripgrep alacritty lldb rustup git-credential-oauth 
 pipx install jrnl
 cargo install tealdeer
 clear
 
+# inicializa o git credential oauth
+git-credential-oauth configure
+git config --global --unset-all credential.helper
+git config --global --add credential.helper "cache --timeout 21600" # seis horas
+git config --global --add credential.helper oauth
+
+# ativa numlock ao iniciar o PC
+sed '2s/.*/numlockx \&/' -i $HOME/.bash_profile
+
 # configura o i3wm
-curl --tlsv1.2 --proto "=https" https://raw.githubusercontent.com/glprojetinho2/dotfiles/main/i3wm/config -Ssf > $HOME/.i3/config
+curl --tlsv1.3 --proto "=https" https://raw.githubusercontent.com/glprojetinho2/dotfiles/main/i3wm/config -Ssf > $HOME/.i3/config
+
+#configura o lvim
+curl --tlsv1.3 --proto "=https" https://raw.githubusercontent.com/glprojetinho2/dotfiles/main/lvim/config.lua -Ssf > $HOME/.config/lvim/config.lua
 
 export NERD_FONT_NAME=0xProto
 # instala o lvim
-curl --tlsv1.2 --proto "=https" https://raw.githubusercontent.com/glprojetinho2/dotfiles/main/lvim/arch_install.sh -Ssf | sh
+curl --tlsv1.3 --proto "=https" https://raw.githubusercontent.com/glprojetinho2/dotfiles/main/lvim/arch_install.sh -Ssf | sh
+
+librewolf --setDefaultBrowser
