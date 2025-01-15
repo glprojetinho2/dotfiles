d483fd3d58f70d0b164643ae2174d8c32dc8ffe7
glprojetinho2 <gr20052512@gmail.com>
2025-01-15 19:41:43 -0300

    atualização

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,11 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-set -U EDITOR helix
-set -U VISUAL helix
+fish_add_path $HOME/.local/bin
+set -gx EDITOR helix
+set -gx VISUAL helix
 set fish_greeting
-set -g fish_key_bindings fish_vi_key_bindings
+set -gx fish_key_bindings fish_vi_key_bindings
 bind -M insert \cf accept-autosuggestion
 
 starship init fish | source
