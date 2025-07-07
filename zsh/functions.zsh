698eec4d77870eef65a31e7909bd40efa01cae89
glprojetinho2 <gr20052512@gmail.com>
2025-07-06 23:08:11 -0300

    atualização

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,4 +1,29 @@
-function y() {
+french()
+{
+  word=$1
+  page="$(curl -sSf "https://fr.wiktionary.org/wiki/$word" | paste -s -d ' ' | perl -pe 's/.+?<h2 id="Français">/<html><body><div><h2 id="Français">/' | sed 's:\\:\\\\:g' | sed 's-"//-"https://-g' | perl -pe 's/\s+/ /g' | perl -pe 's:<a .+?>::g' | perl -pe 's:</a>::g' | perl -pe 's:<b( .+?>|>)([^<]+?)</b>:<a href="${2}">_</a>:g')"
+  echo "$page" | pup '.API text{}' > /tmp/french_ipa
+  pronunciation="$(echo "$page" | pup "source attr{src}" | grep -P '%28fra%29' | head -n1 | sed 's/^/https:/')"
+  echo -e "$(cat /tmp/french_ipa)\n$pronunciation" | wl-copy
+  echo "$page" > /tmp/french.html
+  cat /tmp/french_ipa | sort | uniq
+  echo "$pronunciation"
+}
+
+russian()
+{
+  word=$1
+  page="$(curl -sSf "https://ru.wiktionary.org/wiki/$word" | paste -s -d ' ' | perl -pe 's/.+?<h1 id="Русский">/<html><body><div><h1 id="Русский">/' | sed 's:\\:\\\\:g')"
+  echo "$page" | pup '.IPA text{}' | sed -e 's/^/[/g' -e 's/$/]/g' | xargs > /tmp/russian_ipa
+  pronunciation="$(echo "$page" | pup "source attr{src}" | grep -P '/Ru-' | head -n1 | sed 's/^/https:/')"
+  echo "$page" | sed 's-"//-"https://-g' | perl -pe 's/\s+/ /g' | perl -pe 's:<a .+?>::g' | perl -pe 's:</a>::g' | perl -pe 's:<b( .+?>|>)([^<]+?)</b>:<a href="${2}">_</a>:g' > /tmp/russian.html
+  echo -e "$(cat /tmp/russian_ipa)\n$pronunciation" | wl-copy 
+  cat /tmp/russian_ipa
+  echo "$pronunciation"
+}
+
+y()
+{
 	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
 	yazi "$@" --cwd-file="$tmp"
 	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
@@ -7,7 +32,8 @@ function y() {
 	rm -f -- "$tmp"
 }
 
-toggle_border() {
+toggle_border()
+{
     if [[ "$_border" == "1" ]]; then
         _border=0
         riverctl border-color-focused 0xffffff
@@ -18,7 +44,8 @@ toggle_border() {
     
 }
 
-function esconder() {
+esconder()
+{
    local exata=0
    for arg in "$@"; do
     if [[ "$arg" == "-e" || "$arg" == "--exata" ]]; then
@@ -31,7 +58,7 @@ function esconder() {
   else
     padrao="($1)"
   fi
-  resultado="$(wl-paste | perl -pe "s/$padrao/<span class=\"esconder\" texto=\"\$1\">_<\/span>/g" | perl -pe 's/\n+/<br>/g' | perl -pe 's/<br>$//g')"
+  resultado="$(wl-paste | perl -pe "s/$padrao/<span class=\"esconder\" texto=\"\$1\">_<\/span>/gi" | perl -pe 's/\n+/<br>/g' | perl -pe 's/<br>$//g')"
   echo $resultado | wl-copy
   echo $resultado 
 }
